db8cb9d2eb452b6c2c6e74f4341e82a8086275b2
Alex Bae <alexs39bae@gmail.com>
2024-10-28 11:24:32 -0700

    ...

--- USADemographicsMap/src/Map.jsx
+++ USADemographicsMap/src/Map.jsx
@@ -1,35 +1,14 @@
-// src/Map.js
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const containerStyle = {
-  width: '100%',
-  height: '100vh',
-};
+let map;
 
-const center = {
-  lat: 37.0902, // Latitude for the United States
-  lng: -95.7129, // Longitude for the United States
-};
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
 
-const options = {
-  disableDefaultUI: true, // Disables the default UI for a cleaner look
-  zoomControl: true, // Enables zoom control
-};
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 37.0902, lng: -95.7129 },
+    zoom: 8,
+  });
+}
 
-const Map = () => {
-  return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={4} // Adjust initial zoom level
-        options={options}
-      >
-        { /* Add any markers or overlays here */ }
-      </GoogleMap>
-    </LoadScript>
-  );
-};
-
-export default Map;
+initMap();
