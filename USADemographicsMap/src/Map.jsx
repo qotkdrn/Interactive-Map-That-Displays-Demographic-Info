68e41fa4917f5fbe5c6892f4948b1d0658b16d8e
Alex Bae <alexs39bae@gmail.com>
2024-10-28 14:53:46 -0700

    ...

--- USADemographicsMap/src/Map.jsx
+++ USADemographicsMap/src/Map.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { loadGoogleMapsAPI } from './googleMapsLoader';
 
 let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
-
   map = new Map(document.getElementById("map"), {
     center: { lat: 37.0902, lng: -95.7129 },
     zoom: 8,
   });
 }
 
-initMap();
+const Map = () => {
+  useEffect(() => {
+    loadGoogleMapsAPI();
+    window.initMap = initMap; // Set as global function for callback
+  }, []);
+
+  return <div id="map"></div>;
+};
+
+export default Map;
